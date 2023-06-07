a16278f1ec9e758252cfcad7bd4f1f2a93b92015
unknown <wojcikstw@gmail.com>
2023-06-07 17:58:41 +0200

    first commit

---
+++ src/test/java/stanislaw/wojcik/githubapiconsumer/GithubApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package stanislaw.wojcik.githubapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
